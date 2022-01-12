c8ebf9a79ce94744339c3cacbcd6c565225e14ba
FayxChance <fayxchancetom@outlook.com>
2022-01-12 09:06:31 +0100

    Add NaN on division

--- app/functions.js
+++ app/functions.js
@@ -1,15 +1,16 @@
 exports.sub = (num1, num2) => {
-    return (num1 - num2).toString();
-}
+  return (num1 - num2).toString();
+};
 
 exports.add = (num1, num2) => {
-    return (num1 + num2).toString();
-}
+  return (num1 + num2).toString();
+};
 
 exports.mul = (num1, num2) => {
-    return (num1 * num2).toString();
-}
+  return (num1 * num2).toString();
+};
 
 exports.div = (num1, num2) => {
-    return (num1 / num2).toString();
-}
+  if (num2 == 0) return "NaN";
+  return (num1 / num2).toString();
+};
